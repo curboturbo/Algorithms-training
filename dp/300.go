0ffc002a8c125b17ce9c398e351838876bd0e4ad
Rabbit <arrdin32@gmail.com>
2026-01-30 23:19:32 +0300

    added 300.go

---
+++ dp/300.go
@@ -0,0 +1,22 @@
+package main
+
+func m(x int, y int)int{if x>=y{return x}else{return y}}
+
+
+func lengthOfLIS(nums []int) int {
+	dp:=make([]int, len(nums))
+	for i:=0;i<len(nums);i++{dp[i] = 1}
+	dp[0]=1
+    mx:=-1000
+	for i:=0;i<len(nums);i++{
+		for j:=0;j<i;j++{
+			if nums[j] < nums[i] && dp[i] < dp[j]+1{
+				dp[i] = dp[j]+1
+                mx = m(mx,dp[i])
+			}
+		}
+	}
+	for i:=0;i<len(nums);i++{fmt.Printf("%d ",dp[i])}
+    if mx == -1000{return 1}else{return mx}
+	return mx
+}
